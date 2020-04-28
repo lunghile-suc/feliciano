ea8f738b46b2555a46b041f78b4090e3353c8c4b
lunghile-suc <60696833+lunghile-suc@users.noreply.github.com>
2020-04-28 14:40:04 +0200

    added header contents & styled

---
+++ js/main.js
@@ -0,0 +1,20 @@
+// carousel
+$('.carousel-content').slick({
+    dots: false,
+    fade: true,
+    infinite: true,
+    cssEase: 'linear',
+    autoplay: true,
+    autoplaySpeed: 3000,
+});
+
+// nav toggle
+$('.burger').click(function(){
+    $('ul').slideToggle('active');
+});
+
+// nav scrolled
+$(window).scroll(function(){
+    $('nav').toggleClass('scrolled', $(this).scrollTop()>0);
+    $('ul').toggleClass('scrolled', $(this).scrollTop()>0);
+});
