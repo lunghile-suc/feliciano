e813f2c2a4fbf98b23c3a41611ffa728186bbd8e
lunghile-suc <60696833+lunghile-suc@users.noreply.github.com>
2020-05-01 12:38:29 +0200

    added contact section

--- js/main.js
+++ js/main.js
@@ -1,20 +1,30 @@
-// carousel
-$('.carousel-content').slick({
-    dots: false,
-    fade: true,
-    infinite: true,
-    cssEase: 'linear',
-    autoplay: true,
-    autoplaySpeed: 3000,
-});
+$(document).ready(function () {
+    // carousel
+    $('.carousel-content').slick({
+        dots: false,
+        fade: true,
+        infinite: true,
+        cssEase: 'linear',
+        autoplay: true,
+        autoplaySpeed: 3000,
+    });
 
-// nav toggle
-$('.burger').click(function(){
-    $('ul').slideToggle('active');
-});
+    // nav toggle
+    $('.burger').click(function(){
+        $('ul').slideToggle('active');
+    });
 
-// nav scrolled
-$(window).scroll(function(){
-    $('nav').toggleClass('scrolled', $(this).scrollTop()>0);
-    $('ul').toggleClass('scrolled', $(this).scrollTop()>0);
+    // nav scrolled
+    $(window).scroll(function(){
+        $('nav').toggleClass('scrolled', $(this).scrollTop()>0);
+        $('ul').toggleClass('scrolled', $(this).scrollTop()>0);
+    });
+
+    // timepicker
+    $('#timepicker').timepicker();
+
+    $('#datepicker').datetimepicker({
+        timepicker: false,
+        mask:true,
+    });
 });
